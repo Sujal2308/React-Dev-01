c4ac24050f0fb3df3cdc809af4b9dab3037a3b5c
Sujal2308 <sujalbhugul08@gmail.com>
2025-01-24 18:22:30 +0530

    adding few comments about props object

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,9 +1,12 @@
-const Cards = (props) => {
+const Cards = ({image,title,age}) => {
+  // this is the child component
+  // Technically , props is an object , props = {image:"https://www.w3schools.com/howto/img_avatar.png",title:"Sujal",age:21}
+  // let {image,title,age} = props;
   return (
     <div style={{border:"2px solid black",padding:"10px",margin:"10px",backgroundColor:"lightgreen"}}>
-      <img src = {props.image} width={300}/>
-        <h1>Name : {props.title}</h1>
-        <h2>Age : {props.age}</h2>
+      <img src = {image} width={300}/>
+        <h1>Name : {title}</h1>
+        <h2>Age : {age}</h2>
     </div>
   )
 }
