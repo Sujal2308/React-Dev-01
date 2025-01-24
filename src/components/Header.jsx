0353202485c2bbab7e54551088bcbbffa48dab8a
Sujal2308 <sujalbhugul08@gmail.com>
2025-01-24 18:14:14 +0530

    prop object destructuring

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,15 @@ const Header = () =>{
     let days = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
     let d = new Date().getDay(); // get current day in numbers (0 to 6)
     let time = new Date().toLocaleTimeString(); // get current time
+    return(
+
         <header>
             <h1>Today is : {days[d]}</h1>
             <h2>Time : {time}</h2>
         </header>
+
+    )
+        
     
 }
 export default Header;
