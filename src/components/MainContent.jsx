7f1b242ef669e5b355e61209319e1f08a2f2e17b
Sujal2308 <sujalbhugul08@gmail.com>
2025-01-25 12:28:11 +0530

    updating state via useState

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,8 +1,30 @@
-
+import { useState } from "react"
 const MainContent = () => {
+  // let count = useState(0); returns an array
+  let [count , setCount] = useState(0); //! destructuring the array
+
+  //! function to decrement the value
+  function decrementVal(){
+    setCount(count - 1);
+  }
+
+  //! style object
+  let mainAreaStyle = {
+    color: "blue",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  }
+
+  //? setCount(count+1) causes rerendering of the component
+  console.log("rerendered")
   return (
-    <main style={{color: "blue", display: "flex", justifyContent: "center", alignItems: "center", height: "100vh",}}>
-      <h1 style={{border : "6px solid red" , backgroundColor : "black" }}>This is the main content area.<br/>You can add your components here.</h1>
+    //! inline styling of components
+    <main style={mainAreaStyle}>
+      <h1 style={{border : "6px solid red" , backgroundColor : "black" , padding : "20px", marginRight: "50px"}}>{count}</h1>
+      <button style={{fontSize : "25px"}}  onClick={()=>setCount(count+1)} >Increment</button>
+      <button style={{marginLeft:"20px" , fontSize : "25px"}} onClick={()=>setCount(decrementVal)}>Decrement</button>
     </main>
   )  
 }
