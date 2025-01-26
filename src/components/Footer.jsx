f89ed266c0548a7475cb3e6f69048289e754e489
Sujal2308 <sujalbhugul08@gmail.com>
2025-01-26 19:52:33 +0530

    Copying Clipboard with .then and time function

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,41 @@
 import { useState } from "react";
+import { FaCopy } from "react-icons/fa";
 const Footer = () => {
   let [footerText, setFooterText] = useState("");
-  function updateText(e){
-    footerText = e.target.value;
+  let [headingText, setHeadingText] = useState("");
+  let logo = <FaCopy />;
+
+  function copyText(){
+    navigator.clipboard.writeText(footerText)
+    .then(()=>setHeadingText("Copied"))
+    setTimeout(()=>setHeadingText(""),2000)
   }
   return (
     <footer>
-      <h1>{footerText}</h1>
+     
       <input
         style={{ padding: "25px", fontSize: "23px" }}
         type="text"
-        onChange={updateText}
+        value={footerText}
+        onChange={(e) => setFooterText(e.target.value)}
       />
       <button
         style={{ padding: "25px", fontSize: "23px" }}
-        onClick={() => setFooterText("Hello"+" "+footerText)}
+        onClick={() => setHeadingText("Hello" + " " + footerText)}
       >
         Submit
       </button>
+      <button
+        style={{ padding: "25px", fontSize: "23px" }}
+        onClick={() => setFooterText("")}
+      >
+        Reset
+      </button>
+      <button
+        style={{ padding: "25px", fontSize: "23px" }}
+        onClick= {copyText}
+      >Copy</button>
+       <h1>{headingText}</h1>
     </footer>
   );
 };
